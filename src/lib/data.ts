027284499719c7dec5ef5414b28101047bc13260
akilane <akilane@metaplore.com>
2025-06-16 10:52:04 +0530

    Initial commit

---
+++ src/lib/data.ts
@@ -0,0 +1,150 @@
+export const personalInfo = {
+  name: "Akilan Elangovan",
+  location: "Chennai, India",
+  email: "akilan7745@gmail.com",
+  github: "https://github.com/AKILANELANGOVAN",
+  linkedin: "https://www.linkedin.com/in/akilan-elangovan",
+  resume:'/Akilan_Resume.docx'
+};
+
+export const workExperience = [
+  {
+    company: "Metaplore Solutions Private Limited",
+    location: "Onsite",
+    position: "Software Engineer",
+    period: "Aug 2023 - Present",
+    achievements: [
+      "Engineered a high-performance Angular application for an enterprise product, utilizing Angular Material and a custom-optimized data table built with CDK and third-party libraries, resulting in a 10% reduction in overall bundle size and improved application efficiency.",
+      "Implemented lazy loading and route-level code splitting in Angular applications to optimize performance, reduce initial load time, and improve user experience on large-scale enterprise projects.",
+      "Developed a dynamic, role-based authorization system across the application, leveraging user configurations from the portal to ensure secure and flexible access control.",
+      "Led development of single-page applications (SPAs) using Angular, applying component-based architecture, responsive UI patterns, and performance optimizations to enhance UX across devices.",
+      "Managed client-side multitenancy, implementing dynamic configuration loading via APIs, and enabling theme-based customization per tenant, improving scalability and maintainability.",
+      "Integrated RESTful APIs to enable efficient and reliable data exchange between frontend and backend systems, following SDLC best practices and maintaining consistent API contracts.",
+      "Actively participated in Agile ceremonies such as daily stand-ups, sprint planning, retrospectives, and peer code reviews, ensuring high code quality, collaboration, and continuous delivery.",
+    ]
+  
+
+  },
+  
+   
+  
+];
+
+export const education = [
+  {
+    institution: "Sri Ramakrishna College of Arts & Science",
+    location: "Coimbatore, India",
+    degree: "Bachelor of Computer Applications (BCA)",
+    period: "Jun 2020 - Jun 2023",
+    // achievements: [
+    //   "President of the Department of Information Technology",
+    //   "G20 student delegate",
+    //   "Organized multiple technical workshops and events",
+    //   "Represented my college in multiple national and international hackathons and won.",
+    // ],
+  },
+];
+export const skills = {
+  programmingLanguages: [
+    "TypeScript",
+    "JavaScript",
+    "Python",
+  ],
+  frontendDevelopment: [
+    "Angular",
+    "Reactjs",
+    "Tailwind CSS",
+    "HTML",
+    "CSS",
+  ],
+  backendDevelopment: ["Nodejs",],
+  databaseAndStorage: ["PostgreSQL", "MySQl"],
+  cloudAndDevOps: ["AWS","Snowflake"],
+  toolsAndServices: [
+    "Sentry",
+    "PlayWright",
+    "Git",
+    "Eslint",
+    "Bandit",
+    "WinSCP",
+    "React Flow",
+    "Angular CLI"
+  ],
+};
+
+export const projects = [
+  {
+    title: "Net Zero Carbon Emissions",
+    github: "https://github.com/rishikesh2003/Prodigi",
+    description: [
+      "WiFi-RTT: Developed indoor occupancy tracking for energy optimization.",
+      "IoT Solutions: Implemented smart monitoring for energy efficiency and food waste reduction.",
+      "Real-Time Tracking: Designed systems to monitor carbon emissions and optimize resources.",
+      "Data-Driven Insights: Analyzed user patterns for adaptive energy and food management.",
+      "Reward Integration: Built QR-based green points system to incentivize eco-friendly actions.",
+    ],
+  },
+  {
+    title: "Mental Aarog",
+    github: "https://github.com/rishikesh2003/mental-aarog",
+    description: [
+      "A holistic mental health app leveraging AI and blockchain for early detection and personalized solutions for depression.",
+      "Features include social media analysis (ML-based sentiment tracking), PHQ-9 assessments, smartwatch integration for sleep and activity data, guided meditation, and smart suggestions for food, travel, music, and movies.",
+      "Developed a rewards system using MAG crypto token on Ethereum, enabling user engagement through in-app incentives.",
+      "Secure storage implemented via web3.storage, IPFS, and FileCoin.",
+      "Built with React, Supabase, Node.js, Flask, and Solidity, showcasing seamless integration of health tech and blockchain.",
+    ],
+  },
+];
+
+// export const awards = [
+//   {
+//     name: "IEEE YESIST12 Hackathon",
+//     issuer: "IEEE",
+//     date: "Sep 2022",
+//     type: "International",
+//     position: "Second Place",
+//   },
+//   {
+//     name: "Prodigi Cognizant Hackathon",
+//     issuer: "Cognizant",
+//     date: "Feb 2023",
+//     type: "National",
+//     position: "Second Runner-up",
+//   },
+//   {
+//     name: "Cisco Thingqbator Hackathon",
+//     issuer: "Cisco",
+//     date: "Jan 2023",
+//     type: "National",
+//     position: "First Runner-up",
+//   },
+//   {
+//     name: "Innovators Day",
+//     issuer: "Sri Manakula Vinayagar Engineering College, Pondicherry",
+//     date: "Sep 2022",
+//     type: "National",
+//     position: "First Prize",
+//   },
+//   {
+//     name: "KG Hackfest'22",
+//     issuer: "KGiSL Institute of Technology, Coimbatore",
+//     date: "Sep 2022",
+//     type: "National",
+//     position: "Second Prize",
+//   },
+//   {
+//     name: "Innohacks'22",
+//     issuer: "Innogeeks, KIET Group of Institutions, New Delhi",
+//     date: "May 2022",
+//     type: "National",
+//     position: "Second Runner-up",
+//   },
+//   {
+//     name: "Hack @ SKCET",
+//     issuer: "Hackclub SKCET, SKCET, Coimbatore",
+//     date: "Feb 2022",
+//     type: "National",
+//     position: "Most Impactful Hack",
+//   },
+// ];
